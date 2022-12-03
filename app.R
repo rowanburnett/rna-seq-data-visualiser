15986e50c891d9ed2f7790f865470269d9539671
Rowan Burnett <rowan.sbur@gmail.com>
2022-12-03 15:46:57 +0000

    Fetch data as needed based on input

--- app.R
+++ app.R
@@ -5,16 +5,22 @@ library(tidyverse)
 library(httr)
 library(jsonlite)
 
+# deseq2 file paths
+deseq2_salivary_gland <- "./data/DESEQ2 Results/Salivary Gland/deseq2_salivary_gland.xlsx"
+deseq2_brain <- "./data/DESEQ2 Results/Brain/deseq2_brain.xlsx"
+deseq2_wing_disc <- "./data/DESEQ2 Results/Wing Disc/deseq2_wing_disc.xlsx"
+
 ui <- fluidPage(
   
   titlePanel("Title"),
   
   sidebarLayout(
     sidebarPanel(
-      checkboxGroupInput("genes", 
-                         label = "Genes",
-                         choices = list("Test1" = 1,
-                                        "Test2" = 2))
+      checkboxGroupInput("deseq2", 
+                         label = "DeSEQ2 data",
+                         choices = list("Salivary gland" = deseq2_salivary_gland,
+                                        "Brain" = deseq2_brain,
+                                        "Wing disc" = deseq2_wing_disc))
     ),
     
     mainPanel(
@@ -32,16 +38,18 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-
-  PtcG4_D6_V_Fer12WT_D6 = read_excel("./data/DESEQ2 Results/Salivary Gland/deseq2_salivary_gland.xlsx", sheet = 1, range = "A1:G50") %>%
-                          select(-baseMean, -lfcSE, -stat, -pvalue)
-  PtcG4_D6_V_RasYki_D5 = read_excel("./data/DESEQ2 Results/Salivary Gland/deseq2_salivary_gland.xlsx", sheet = 2, range = "A1:G50") %>%
-                          select(-baseMean, -lfcSE, -stat, -pvalue)
-  PtcG4_D6_V_RasYki_D8 = read_excel("./data/DESEQ2 Results/Salivary Gland/deseq2_salivary_gland.xlsx", sheet = 3, range = "A1:G50") %>%
-                          select(-baseMean, -lfcSE, -stat, -pvalue)
   
+  get_data <- function(path) {
+    print(path)
+    data <- read_excel(path, 1, na = "NA") %>%
+      select(-baseMean, -lfcSE, -stat, -pvalue) %>%
+      replace(is.na(.), 0)
+  }
+                        
   output$volcanoPlot <- renderPlot({
-    ggplot(data = PtcG4_D6_V_Fer12OG_D6, aes(x = log2FoldChange, y = -log10(padj))) + 
+    deseq2_data <- lapply(input$deseq2, get_data)
+    deseq2_data <- bind_rows(deseq2_data)
+    ggplot(data = deseq2_data, aes(x = log2FoldChange, y = -log10(padj))) + 
       geom_point() +
       theme_minimal() +
       geom_vline(xintercept=c(-1.4, 1.4), col="red") +
