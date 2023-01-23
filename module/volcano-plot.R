6d7ae03111b7cf6832532f93f62e1c350dd93ebc
rowanburnett <rowan.sbur@gmail.com>
2023-01-23 20:54:49 +0000

    Generate plots in server function

--- module/volcano-plot.R
+++ module/volcano-plot.R
@@ -8,20 +8,24 @@ volcanoPlotUI <- function(id, label = "Volcano plot") {
                            label = "Genotypes",
                            choiceNames = genotype_names,
                            choiceValues = genotype_values),
+        
         checkboxGroupInput(ns("tissues"),
                            label = "Tissues",
                            choiceNames = tissue_names,
                            choiceValues = tissue_values),
+        
         numericInput(ns("pvalue"),
                      label = "p-value",
                      value = 0.05,
                      min = 0,
                      step = 0.01),
+        
         numericInput(ns("log2foldchange"),
                      label = "Log2 fold change",
                      value = 1.4,
                      min = 0,
                      step = 0.01),
+        
         fileInput(ns("file"),
                   "Upload CSV or Excel workbook",
                   buttonLabel = "Browse...",
@@ -30,11 +34,8 @@ volcanoPlotUI <- function(id, label = "Volcano plot") {
       
       dashboardBody(
         fluidRow(
-          tabBox(
-            id = ns("tabset"),
-            title = "Volcano plot"
-          ),
-        
+          htmlOutput(ns("plots")),
+          
           box(
             title = "Extra information",
             htmlOutput(ns("geneInfo"))
@@ -54,13 +55,18 @@ volcanoPlotServer <- function(id) {
     id,
     
     function(input, output, session) {
+      # need to use session namespace for ui elements created in server function
+      ns <- session$ns
+      
       # increase max request size to upload files up to 100mb
       options(shiny.maxRequestSize=100*1024^2)
       
       # get gene symbols from gene ids
-      get_gene_names <- function(data) {
-        symbols <- mapIds(org.Dm.eg.db, keys = data$id, keytype = "FLYBASE", column = "SYMBOL")
-        unname(symbols)
+      get_gene_names <- function(df) {
+        symbols <- mapIds(org.Dm.eg.db, 
+                          keys = df$id, 
+                          keytype = "FLYBASE", 
+                          column = "SYMBOL")
       }
       
       # get data from excel files based on provided parameters
@@ -71,34 +77,47 @@ volcanoPlotServer <- function(id) {
           mutate("-log10(padj)" = -log10(padj))
       }
       
+      # render plots dynamically 
       observeEvent(data(), {
-        print(data())
-        for (data in data()) {
-            appendTab("tabset",
-                        tabPanel("Tab",
-                          renderPlot({
-                            EnhancedVolcano(data, lab = "id", x = "log2FoldChange", y = "padj",
-                                               pCutoff = input$pvalue,
-                                               FCcutoff = input$log2foldchange)
-                             })
-                        )
-                      )
-                    }
+        lapply(seq(data()), function(i) {
+          output[[paste0("plot", i) ]] <- renderPlot({
+            data <- data()[[i]]
+            data <- mutate(data, "symbol" = get_gene_names(data))
+            print(head(data))
+            EnhancedVolcano(data, lab = data$symbol, x = "log2FoldChange", y = "padj",
+                            pCutoff = input$pvalue,
+                            FCcutoff = input$log2foldchange)
+          })
+        })
       })
       
+      output$plots <- renderUI ({
+        do.call(tabBox, c(lapply(seq(data()), function(i) {
+          tabPanel(
+            title = names(data()[i]),
+            plotOutput(ns(paste0("plot", i)), 
+                       click = ns(paste0("plot_click", i)))
+          )
+        })))
+      })
+      
+      # gather tissues and genotypes to get from excel files
       data <- reactive({
-        deseq2_data <- data.frame()
+        data <- list()
 
-        # gather tissues and genotype to get from excel files - this isn't very good
         for (tissue in input$tissues) {
           for (genotype in input$genotypes) {
             tryCatch({
               sheets <- excel_sheets(tissue)
               for (sheet in sheets) {
-                index <- which(tissue_values == tissue)
-                name <- tissue_names[index]
+                
+                # get tissue name for plot title
+                tissue_index <- which(tissue_values == tissue)
+                tissue_name <- tissue_names[tissue_index]
+                
+                # check if genotype in sheet name - need to do this a better way
                 if (grepl(genotype, sheet)) {
-                  deseq2_data <- append(deseq2_data, lapply(tissue, get_data, sheet = sheet))
+                  data[[paste0(tissue_name, " ", genotype)]] <- get_data(tissue, sheet)
                 }
               }
             },
@@ -107,74 +126,56 @@ volcanoPlotServer <- function(id) {
             })
           }
         }
-        return(deseq2_data)
+        return(data)
       })
       
-      
-      output$volcanoPlot <- renderPlot({
-        req(input$tissues, input$genotypes)
-
-        # create volcano plot
-         EnhancedVolcano(data(), lab = data()$symbol, x = "log2FoldChange", y = "padj",
-           pCutoff = input$pvalue,
-           FCcutoff = input$log2foldchange)
+      # extra gene info box
+      output$geneInfo <- renderUI({
+        lapply(seq(data()), function(i) {
+          gene <<- nearPoints(
+            data()[[i]], 
+            input[[paste0("plot_click", i)]],
+            xvar = "log2FoldChange",
+            yvar = "-log10(padj)",
+            maxpoints = 1
+          )
+        })
+        
+        if (length(gene$id) > 0) {
+          summ = summary(gene$id)
+        
+          tagList(
+            div(
+              span("Symbol:", style = "font-weight: bold;"), 
+              span(gene$symbol)),
+            div(
+              span("ID:", style = "font-weight: bold;"), 
+              span(gene$id)),
+            div(
+              span("Log2 fold change:", style = "font-weight: bold;"), 
+              span(gene$log2FoldChange)),
+            div(
+              span("p-value:", style = "font-weight: bold;"), 
+              span(gene$padj)),
+            div(
+              span("Gene summary:", style = "font-weight: bold;"), 
+              span(summ))
+          )
+        }
       })
       
-      point <- function(){
-        deseq2_data <- data()
-        gene <- nearPoints(
-          deseq2_data, 
-          input$plot_click,
-          xvar = "log2FoldChange",
-          yvar = "-log10(padj)",
-          maxpoints = 1
-        )
-      }
-      
+      # get gene summary from flybase api
       summary <- function(id) {
         res <- GET(glue("https://api.flybase.org/api/v1.0/gene/summaries/auto/{id}"))
         summary <- fromJSON(rawToChar(res$content))
         summary <- summary$resultset$result$summary
       }
       
-      output$geneInfo <- renderUI({
-        gene <- nearPoints(
-          data(), 
-          input$plot_click,
-          xvar = "log2FoldChange",
-          yvar = "-log10(padj)",
-          maxpoints = 1
-        )
-        
-        if (length(gene$id) > 0) {
-          summ = summary(gene$id)
-        
-        tagList(
-          div(
-            span("Symbol:", style = "font-weight: bold;"), 
-            span(gene$symbol)),
-          div(
-            span("ID:", style = "font-weight: bold;"), 
-            span(gene$id)),
-          div(
-            span("Log2 fold change:", style = "font-weight: bold;"), 
-            span(gene$log2FoldChange)),
-          div(
-            span("p-value:", style = "font-weight: bold;"), 
-            span(gene$padj)),
-          div(
-            span("Gene summary:", style = "font-weight: bold;"), 
-            span(summ))
-        )
-      }
-      })
-      
       output$fileInfo <- renderPrint({
         file <- input$file
         req(file)
         print(file)
         ext <- tools::file_ext(file$datapath)
-        
         
         if (ext == "csv") {
           print("csv file")
@@ -184,5 +185,4 @@ volcanoPlotServer <- function(id) {
         
       })
     }
-
 )}
