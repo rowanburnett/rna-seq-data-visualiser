6b8d51418b09142e10ff54742c983755b24ca387
rowanburnett <rowan.sbur@gmail.com>
2023-01-29 14:25:33 +0000

    Fix plot and gene info box re-rendering on plot click

--- module/volcano-plot.R
+++ module/volcano-plot.R
@@ -23,7 +23,8 @@ volcanoPlotUI <- function(id, label = "Volcano plot") {
                    value = 10,
                    min = 1,
                    step = 0.01),
-      textAreaInput(ns("gene_list"),
+      
+      textAreaInput(ns("geneList"),
                     label = "Enter a list of gene symbols to label",
                     placeholder = "")
     ),
@@ -42,6 +43,9 @@ volcanoPlotServer <- function(id, dataset) {
     function(input, output, session) {
       # need to use session namespace for ui elements created in server function
       ns <- session$ns
+      
+      obs <- list()
+      val <- reactiveValues()
 
       output$dataChoices <- renderUI({
         tagList(
@@ -65,8 +69,36 @@ volcanoPlotServer <- function(id, dataset) {
                                  tools::file_path_sans_ext(file), 
                                  choices = choices)
             }
-        })
+          })
         )
+      })
+      
+      data <- reactive({
+        dataList <- list()
+        lapply(dataset(), function(data) {
+          for (file in input[[data]]) {
+            ext <- tools::file_ext(data) # file extension
+            
+            df <- data.frame()
+            # different read functions depending on file format
+            if (ext == "xls" || ext == "xlsx") {
+              df <- read_excel(paste0("./data/Uploads/", data), file, na = "na")
+              
+            } else if (ext == "csv") {
+              df <- read.csv(paste0("./data/Uploads/", data))
+            }
+            
+            df[,-1] <- lapply(df[,-1], as.numeric)
+            colnames(df)[1] <- "id"
+            df <- dplyr::select(df, log2FoldChange, padj, id) %>%
+              mutate("-log10(padj)" = -log10(padj)) %>%
+              na.omit()
+            df <- mutate(df, "symbol" = get_gene_names(df))
+            
+            dataList[[paste0(data, " ", file)]] <<- df
+          }
+        })
+        return(dataList)
       })
       
       # get gene symbols from gene ids
@@ -77,30 +109,80 @@ volcanoPlotServer <- function(id, dataset) {
                           column = "SYMBOL")
       }
       
-      # genes to label on the plot (optional)
-      geneList <- reactive({
-        geneList <- as.list(el(strsplit(input$gene_list, " ")))
-      })
-      
       # render plots dynamically 
-      observeEvent(data(), {
-        lapply(seq(data()), function(i) {
+      observe({
+        lapply(seq(data()), function(i){
           output[[paste0("plot", i) ]] <- renderPlot({
             data <- data()[[i]]
+
+            # list of genes to label on the plot (optional)
+            geneList <- as.list(el(strsplit(input$geneList, " ")))
             
             # if list of genes is provided then use it
-            if (length(geneList()) > 0) {
-              labelledGenes <<- geneList()
+            if (length(geneList) > 0) {
+              labelledGenes <<- isolate(geneList())
             } else {
               labelledGenes <<- NULL
             }
             
+            # draw volcano plot
             EnhancedVolcano(data, lab = data$symbol, x = "log2FoldChange", y = "padj",
                             pCutoff = input$pvalue,
                             FCcutoff = input$log2foldchange,
                             xlim = c(-input$lfcLimit, input$lfcLimit),
                             selectLab = labelledGenes)
           })
+        })
+      })
+      
+      # extra gene information
+      observe({
+        lapply(seq(data()), function(i){
+          id <- paste0("plot_click",i)
+          
+          # necessary to allow clicking multiple plots correctly
+          if (!is.null(obs[[id]])) {
+            obs[[id]]$destroy()
+          }
+          val[[id]] <- NULL
+          obs[[id]] <<- observeEvent(input[[id]], {
+            data <- data()
+            tryCatch({
+              gene <- nearPoints(
+                data[[i]], 
+                input[[paste0("plot_click", i)]],
+                xvar = "log2FoldChange",
+                yvar = "-log10(padj)",
+                maxpoints = 1
+              )
+              summ <- summary(gene$id)
+              
+              output$geneInfo <- renderUI(
+                tagList(
+                  div(
+                    span("Symbol:", style = "font-weight: bold;"),
+                    span(gene$symbol)),
+                  div(
+                    span("ID:", style = "font-weight: bold;"),
+                    span(gene$id)),
+                  div(
+                    span("Log2 fold change:", style = "font-weight: bold;"),
+                    span(gene$log2FoldChange)),
+                  div(
+                    span("p-value:", style = "font-weight: bold;"),
+                    span(gene$padj)),
+                  div(
+                    span("Gene summary:", style = "font-weight: bold;"),
+                    span(summ))
+                )
+              )
+            }, error = function(cond) {
+              print("No gene selected")
+            })
+            
+            val[[id]] <- input[[id]]$x
+          }, ignoreInit = TRUE)
+          
         })
       })
       
@@ -114,72 +196,9 @@ volcanoPlotServer <- function(id, dataset) {
         })))
       })
       
-      data <- reactive({
-        dataList <- list()
-        lapply(dataset(), function(data) {
-          for (file in input[[data]]) {
-            ext <- tools::file_ext(data)
-            
-            df <- data.frame()
-            if (ext == "xls" || ext == "xlsx") {
-              df <- read_excel(paste0("./data/Uploads/", data), file, na = "na")
-              
-            } else if (ext == "csv") {
-              df <- read.csv(paste0("./data/Uploads/", data))
-            }
-            
-            df[,-1] <- lapply(df[,-1], as.numeric)
-            colnames(df)[1] <- "id"
-            df <- dplyr::select(df, -baseMean, -lfcSE, -stat) %>%
-              mutate("-log10(padj)" = -log10(padj)) %>%
-              na.omit()
-            df <- mutate(df, "symbol" = get_gene_names(df))
-              print(df)
-              
-            dataList[[paste0(data, " ", file)]] <<- df
-          }
-        })
-        return(dataList)
-      })
-      
-      # extra gene info box
-      output$geneInfo <- renderUI({
-        lapply(seq(data()), function(i) {
-          gene <<- nearPoints(
-            data()[[i]], 
-            input[[paste0("plot_click", i)]],
-            xvar = "log2FoldChange",
-            yvar = "-log10(padj)",
-            maxpoints = 1
-          )
-        })
-        
-        if (length(gene$id) > 0) {
-          summ = summary(gene$id)
-        
-          tagList(
-            div(
-              span("Symbol:", style = "font-weight: bold;"), 
-              span(gene$symbol)),
-            div(
-              span("ID:", style = "font-weight: bold;"), 
-              span(gene$id)),
-            div(
-              span("Log2 fold change:", style = "font-weight: bold;"), 
-              span(gene$log2FoldChange)),
-            div(
-              span("p-value:", style = "font-weight: bold;"), 
-              span(gene$padj)),
-            div(
-              span("Gene summary:", style = "font-weight: bold;"), 
-              span(summ))
-          )
-        }
-      })
-      
       # get gene summary from flybase api
       summary <- function(id) {
-        res <- GET(glue("https://api.flybase.org/api/v1.0/gene/summaries/auto/{id}"))
+        res <- GET(paste0("https://api.flybase.org/api/v1.0/gene/summaries/auto/", id))
         summary <- fromJSON(rawToChar(res$content))
         summary <- summary$resultset$result$summary
       }
